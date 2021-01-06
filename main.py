ead8c5cdbec3e47adea38b206290636841713d76
Your Name <you@example.com>
2021-01-06 22:52:46 +0300

    added extra features

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, MetaData, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.elements import RollbackToSavepointClause
+
+import datetime
 
 
 # engine = create_engine('mysql+pymysql://root:@localhost/tasks')
-engine = create_engine('sqlite:///college.db')
+engine = create_engine('sqlite:///college.db',
+                       connect_args={"check_same_thread": False})
 meta = MetaData()
 Base = declarative_base()
 
@@ -26,13 +28,24 @@ class User(Base):
     role = Column('role', String(50))
 
 
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    name = Column('name', String(50))
+    task_id = Column(Integer, ForeignKey('task.id'))
+
+
 class Task(Base):
     __tablename__ = 'task'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     name = Column('name', String(50))
     description = Column('description', String(60))
-    # progress = Column('progress', String(30))
+    progress = Column('progress', String(30))
+    created_date = Column('created_date', DateTime,
+                          default=datetime.datetime.utcnow)
+    due_date = Column('due_date', DateTime)
+    comments = relationship("Comment")
     # status = Column('status', String(50))
     # due_date = Column('due_date', String(30))
     # task_type = Column('task_type', String(30))
@@ -81,6 +94,7 @@ def home():
             users = s.query(User).filter(
                 User.id == session['id']).all()
             if len(users) > 0:
+                # print(users[0].id)
                 return render_template('index.html', user=users[0])
             else:
                 return render_template('login.html')
@@ -149,6 +163,41 @@ def delete_task(id):
     return redirect(url_for('home'))
 
 
+@app.route('/tasks/edit/<id>', methods=['GET'])
+def show_edit(id):
+    task = s.query(Task).filter(
+        Task.id == id).all()[0]
+    role = session['role']
+    return render_template('edit_tasks.html', task=task, role=role)
+
+
+@app.route('/tasks/view/', methods=['GET'])
+def show_tasks():
+    tasks = s.query(Task).filter()
+    if session['role'] == "admin":
+        return render_template('view_tasks.html', tasks=tasks)
+    else:
+        return render_template('all_tasks.html', tasks=tasks, id=session['id'])
+
+
+@app.route('/tasks/edit/<id>', methods=['POST'])
+def edit_task(id):
+    s = db_session()
+    task = s.query(Task).filter(
+        Task.id == id).all()[0]
+    name = request.form['name']
+    description = str(request.form['desc'])
+    progress = str(request.form['prog'])
+    user_id = str(request.form['id'])
+    task.name = name
+    task.description = description
+    task.progress = progress
+    task.user_id = user_id
+    s.add(task)
+    s.commit()
+    return redirect(url_for('home'))
+
+
 @app.route('/tasks/<uid>')
 def tasks_add(uid):
     return render_template('tasks.html')
@@ -159,7 +208,11 @@ def tasks_insert(uid):
     if request.method == "POST":
         name = request.form['name']
         description = str(request.form['desc'])
-        task = Task(name=name, description=description, user_id=uid)
+        due_date = datetime.datetime.strptime(
+            request.form['date'], '%Y-%m-%d')
+        print(type(due_date))
+        task = Task(name=name, description=description,
+                    user_id=uid, progress='nothing yet', due_date=due_date)
         s = db_session()
         s.add(task)
         s.commit()
@@ -167,29 +220,51 @@ def tasks_insert(uid):
     return redirect(url_for('home'))
 
 
-@app.route('/course')
-def course_index():
-    return render_template('course.html')
+@app.route('/comments/<tid>', methods=['POST'])
+def comments_insert(tid):
+    if request.method == "POST":
+        name = request.form['name']
+        task = Comment(name=name, task_id=tid)
+        s = db_session()
+        s.add(task)
+        s.commit()
+        print(task)
+    return redirect(url_for('home'))
 
 
-@app.route('/course', methods=['POST'])
-def course_create():
-    title = request.form['title']
-    description = request.form['description']
-    course = Course(title=title, description=description)
+@app.route('/comments/del/<id>', methods=['GET'])
+def delete_comment(id):
     s = db_session()
-    s.add(course)
+    task = s.query(Comment).filter(
+        Comment.id == id).all()[0]
+    s.delete(task)
     s.commit()
-    print('add')
-    return render_template('course.html')
+    return redirect(url_for('home'))
 
 
-@app.route('/allcourse')
-def course_all():
-    s = db_session()
-    courses = s.query(Course).filter().all()
-    print(courses)
-    return render_template('courses.html', all_course=courses)
+# @app.route('/course')
+# def course_index():
+#     return render_template('course.html')
+
+
+# @app.route('/course', methods=['POST'])
+# def course_create():
+#     title = request.form['title']
+#     description = request.form['description']
+#     course = Course(title=title, description=description)
+#     s = db_session()
+#     s.add(course)
+#     s.commit()
+#     print('add')
+#     return render_template('course.html')
+
+
+# @app.route('/allcourse')
+# def course_all():
+#     s = db_session()
+#     courses = s.query(Course).filter().all()
+#     print(courses)
+#     return render_template('courses.html', all_course=courses)
 
 
 if __name__ == "__main__":
